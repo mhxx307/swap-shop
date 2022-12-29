917d4a4ef45d18e118d8f1040f73d134263800d3
mhxx307 <minhquan.lavo@gmail.com>
2022-12-29 23:09:26 +0700

    :wrench: configuration: prettier, tailwind css, font family, primary colors, set up to use dark theme

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+module.exports = {
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx}',
+        './src/components/**/*.{js,ts,jsx,tsx}',
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                primary: colors.red,
+            },
+        },
+        screens: {
+            xs: '480px',
+            ss: '640px',
+            sm: '768px',
+            md: '1024px',
+            lg: '1280px',
+            xl: '1536px',
+        },
+        fontFamily: {
+            sevillana: ['Sevillana', 'cursive'],
+            lora: ['Lora', 'serif'],
+            timeNewRoman: ['Times New Roman', 'serif'],
+        },
+    },
+    plugins: [],
+};
