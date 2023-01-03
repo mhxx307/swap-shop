9e088e3ff720c73338a0d1c8f5ea129a7c1c2283
mhxx307 <minhquan.lavo@gmail.com>
2023-01-03 17:43:58 +0700

    login: :zap: show hide password

--- src/components/features/auth/login/LoginForm.tsx
+++ src/components/features/auth/login/LoginForm.tsx
@@ -1,7 +1,11 @@
-import { Button, InputField } from '@/components/shared';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { Button, InputField } from '@/components/shared';
 
 const LoginForm = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
     const { control, handleSubmit } = useForm({
         defaultValues: {
             email: '',
@@ -15,8 +19,26 @@ const LoginForm = () => {
 
     return (
         <form method="POST" onSubmit={handleSubmit(handleLogin)}>
-            <InputField type="text" name="email" control={control} />
-            <InputField type="password" name="password" control={control} />
+            <InputField
+                type="text"
+                name="email"
+                control={control}
+                className="px-3 py-2"
+                label="Email"
+            />
+
+            <InputField
+                type={showPassword ? 'text' : 'password'}
+                name="password"
+                control={control}
+                className="px-3 py-2"
+                containerClassName="mt-[8px]"
+                label="Password"
+                iconClassName="w-8 h-8 cursor-pointer hover:text-gray-500"
+                rightIconOnClick={() => setShowPassword(!showPassword)}
+                RightIcon={showPassword ? FaEye : FaEyeSlash}
+            />
+
             <Button type="submit" primary className="mt-[20px]">
                 Login
             </Button>
