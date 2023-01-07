cc1022a272900183d6a8dd4ad8898dfed91a60a9
hoangcobain <hoang21062001@gmail.com>
2023-01-07 21:14:50 +0700

    refactor: 💡 support tranlate for footer
    devide to vi.ts and en .ts
    
    ✅ Closes: #2

--- src/constants/vi.ts
+++ src/constants/vi.ts
@@ -4,13 +4,13 @@ import { BsGear } from 'react-icons/bs';
 import { VscSignOut } from 'react-icons/vsc';
 import { HeaderNavListProps, PopupMenuItemProps } from '@/types';
 
-export const HEADER_NAV_LIST: HeaderNavListProps[] = [
+const HEADER_NAV_LIST: HeaderNavListProps[] = [
     { path: '/', label: 'Trang chủ' },
     { path: '/products', label: 'Giao dịch đồ cũ' },
     { path: '/about', label: 'Về chúng tôi' },
 ];
 
-export const POPUP_MENU_LIST: PopupMenuItemProps[] = [
+const POPUP_MENU_LIST: PopupMenuItemProps[] = [
     {
         icon: AiOutlineQuestionCircle,
         title: 'Phản hồi và giúp đỡ',
@@ -19,35 +19,43 @@ export const POPUP_MENU_LIST: PopupMenuItemProps[] = [
     { icon: CgShortcut, title: 'Phím tắt', to: '/shortcuts' },
 ];
 
-export const POPUP_USER_MENU_LIST: PopupMenuItemProps[] = [
+const POPUP_USER_MENU_LIST: PopupMenuItemProps[] = [
     { icon: AiOutlineUser, title: 'Thông tin tài khoản', to: '/@hoa' },
     { icon: BsGear, title: 'Cài đặt', to: '/settings' },
     ...POPUP_MENU_LIST,
     { icon: VscSignOut, title: 'Đăng xuất', to: '/logout', separate: true },
 ];
 
-export const CUSTOMER_FOOTER_NAV_LIST = [
+const CUSTOMER_FOOTER_NAV_LIST = [
     { path: '/asd', label: 'Chăm sóc khách hàng' },
     { path: '/products', label: 'Trung tâm trợ giúp' },
     { path: '/about', label: 'Chính sách bảo hành' },
 ];
 
-export const ABOUT_FOOTER_NAV_LIST = [
+const ABOUT_FOOTER_NAV_LIST = [
     { path: '/asd', label: 'Về chúng tôi' },
     { path: '/products', label: 'Điều khoản Swap Shop' },
     { path: '/about', label: 'Liên hệ với chúng tôi' },
 ];
 
-export const SOCIAL_FOOTER_NAV_LIST = [
+const SOCIAL_FOOTER_NAV_LIST = [
     { path: '/asd', label: 'Theo dõi chúng tôi trên' },
     { path: '/products', label: 'Facebook' },
     { path: '/about', label: 'Instagram' },
+];
+
+const FOOTER_LIST = [
+    ABOUT_FOOTER_NAV_LIST,
+    CUSTOMER_FOOTER_NAV_LIST,
+    SOCIAL_FOOTER_NAV_LIST,
+    SOCIAL_FOOTER_NAV_LIST,
 ];
 
 const viTranslations = {
     HEADER_NAV_LIST,
     POPUP_MENU_LIST,
     POPUP_USER_MENU_LIST,
+    FOOTER_LIST,
 };
 
 export default viTranslations;
