b2137d79a2fae9a9b9080278cfd73f1312df980f
mhxx307 <minhquan.lavo@gmail.com>
2023-01-30 02:44:58 +0700

    style: 💄 style for btn prev, next swiper
    rounded, hover, width, height to better

--- src/components/features/articles/ArticlesSwiperInfinite.tsx
+++ src/components/features/articles/ArticlesSwiperInfinite.tsx
@@ -6,7 +6,7 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 // import required modules
-import { Pagination, Navigation } from 'swiper';
+import { Navigation } from 'swiper';
 import ArticleCard from './ArticleCard';
 
 interface ArticlesSwiperInfiniteProps {
@@ -38,11 +38,8 @@ const ArticlesSwiperInfinite = ({
                 slidesPerGroup={3}
                 loop={false}
                 loopFillGroupWithBlank={true}
-                pagination={{
-                    clickable: true,
-                }}
                 navigation={true}
-                modules={[Pagination, Navigation]}
+                modules={[Navigation]}
             >
                 {articleList.map((article: any) => (
                     <SwiperSlide key={article.id}>
